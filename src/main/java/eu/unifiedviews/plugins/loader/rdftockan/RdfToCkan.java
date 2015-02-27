a0d04aedece36005ce01cbf36513aa62b0d11397
Michal Klempa <mkl@eea.sk>
2015-02-27 12:46:18 +0100

    Removing appendNumber

--- src/main/java/eu/unifiedviews/plugins/loader/rdftockan/RdfToCkan.java
+++ src/main/java/eu/unifiedviews/plugins/loader/rdftockan/RdfToCkan.java
@@ -248,27 +248,4 @@ public class RdfToCkan extends ConfigurableBase<RdfToCkanConfig_V1> implements C
 
         return resourceBuilder;
     }
-
-    public static String appendNumber(long number) {
-        String value = String.valueOf(number);
-        if (value.length() > 1) {
-            // Check for special case: 11 - 13 are all "th".
-            // So if the second to last digit is 1, it is "th".
-            char secondToLastDigit = value.charAt(value.length() - 2);
-            if (secondToLastDigit == '1') {
-                return value + "th";
-            }
-        }
-        char lastDigit = value.charAt(value.length() - 1);
-        switch (lastDigit) {
-            case '1':
-                return value + "st";
-            case '2':
-                return value + "nd";
-            case '3':
-                return value + "rd";
-            default:
-                return value + "th";
-        }
-    }
 }
